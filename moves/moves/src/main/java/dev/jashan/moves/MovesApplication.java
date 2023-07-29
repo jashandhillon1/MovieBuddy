19e56038a0405ac105c58a23fd6814aeb3677174
jashandhillon1 <jashandhillon211@gmail.com>
2023-07-29 00:54:44 -0400

    database setup

---
+++ moves/moves/src/main/java/dev/jashan/moves/MovesApplication.java
@@ -0,0 +1,18 @@
+package dev.jashan.moves;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class MovesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MovesApplication.class, args);
+	}
+
+
+
+}
---
+++ moves/moves/src/test/java/dev/jashan/moves/MovesApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.jashan.moves;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
