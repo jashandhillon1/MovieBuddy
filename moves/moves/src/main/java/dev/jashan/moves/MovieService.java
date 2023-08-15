4c0e40a51e0e8f7049962d4a5728c0555fd00f51
jashandhillon1 <jashandhillon211@gmail.com>
2023-08-15 15:46:23 -0400

    Backend Complete

--- moves/moves/src/main/java/dev/jashan/moves/MovieService.java
+++ moves/moves/src/main/java/dev/jashan/moves/MovieService.java
@@ -1,9 +1,11 @@
 package dev.jashan.moves;
 
+import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class MovieService {
@@ -12,6 +14,11 @@ public class MovieService {
     private MovieRepository movieRepository;
     public List<Movie> allMovies(){
         return movieRepository.findAll();
+
+    }
+
+    public Optional<Movie> singleMovie(String id){
+        return movieRepository.findMovieByImdbId(id);
 
     }
 }
--- moves/moves/src/main/java/dev/jashan/moves/Review.java
+++ moves/moves/src/main/java/dev/jashan/moves/Review.java
@@ -20,5 +20,7 @@ public class Review {
 
     private String body;
 
-
+    public Review(String body) {
+        this.body = body;
+    }
 }
